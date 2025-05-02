dc8785dc8d4ea8c1780c89f12a960a948c3d4ba2
Dasun1237 <pushpartx7@gmail.com>
2025-05-02 22:44:13 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,23 @@
+module.exports = {
+    audioUrl: 'https://github.com/drsde/send-voice/raw/refs/heads/main/public/DEXTER.mp3',
+    photoUrl: 'https://firebasestorage.googleapis.com/v0/b/testing-766a5.appspot.com/o/storage%2Fdata%2F20240901_041352.jpg?alt=media&token=10aff546-9978-490c-9639-5a0293bd92a7',
+    generalMessage: `*╭────────────⊶*
+*│* *ɪ ᴀᴍ ʀᴇᴀʟ ᴅᴇxᴛᴇʀ*
+*╰────────────⊶*
+
+*╭────────────⊶*
+*│* ○ \`ɴᴀᴍᴇ │ DEXTER\`
+*│* ○ \`ꜰʀᴏᴍ │ᴀᴍᴘᴀʀᴀ\`
+*│* ○ \`ᴀɢᴇ │+17\`
+*╰─────────────⊶*
+*────────────⊶*
+*╭──────────⊶*
+*│❮ මාව save කර ගන්න❯*
+*│"ඔයාව SAVE"*
+*│❮ AUTO REPLY ❯*
+*╰──────────⊶*
+
+*❮ꜱᴀᴠᴇ නම් අනිවරෙන් කියන්න❯*
+*❮STATUS VIEWERS වලට අවේ❯*`,
+    groupLink: 'https://chat.whatsapp.com/FYsbo9QWv2K6wEjN7plbmg'
+};
